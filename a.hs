ed48f43e8b36f4b0ab478757f1b5aeeab6a404ad
oisdk <oisin.kidney@gmail.com>
2016-01-23 18:40:45 +0000

    some cofree

--- a.hs
+++ a.hs
@@ -9,7 +9,8 @@ import Text.PrettyPrint
 import Data.Functor (void)
 import Control.Arrow hiding ((<+>))
 import Data.Maybe (fromMaybe)
-import Control.Monad (join)
+import Control.Comonad.Cofree
+import Control.Comonad
 -- zygo :: (Base t b -> b) -> (Base t (b, a) -> a) -> t -> a
 -- para :: (Base t (t, a)  -> a) -> t -> a
 -- cata :: (Base t a -> a) -> t -> a
@@ -46,45 +47,46 @@ instance Num Expr where
   fromInteger = Expr . ana CstF
   negate      = Expr . Fix . NegF . getExpr
   
-newtype AnnF f a r = AnnF { getAnn :: (f r, a) } deriving (Functor, Eq, Ord)
-type Ann f a = Fix (AnnF f a)
+type Ann f a = Cofree f a
 
-newtype NamedExpr = Named { getNamed :: Ann ExprF (Maybe String) }
+newtype NamedExpr = Named { getNamed :: Cofree ExprF (Maybe String) }
 
-stripAll :: Functor f => Ann f a -> Fix f
-stripAll = cata alg where alg (AnnF (x,_)) = Fix x
+stripAll :: Functor f => Cofree f a -> Fix f
+stripAll = ana unwrap
 
-named :: String -> NamedExpr -> NamedExpr
-named s (Named (Fix (AnnF (e, _)))) = (Named . Fix . AnnF) (e, Just s)
+   
 
-unnamed :: ExprF (Ann ExprF (Maybe String)) -> NamedExpr
-unnamed e = (Named . Fix . AnnF) (e, Nothing)
-
-evalName :: NamedExpr -> Integer
-evalName = eval . Expr . stripAll . getNamed
-
-instance Num NamedExpr where
-  Named a + Named b = unnamed (SumF a b)
-  Named a * Named b = unnamed (PrdF a b)
-  fromInteger = unnamed . CstF
-  abs e | evalName e < 0 = negate e
-        | otherwise      = e
-  signum = fromInteger . signum . evalName
-  negate = unnamed . NegF . getNamed
-  
-opto :: Functor f => (a -> Maybe b) -> (f b -> b) -> Ann f a -> b
-opto ann alg = cata coalg where
-  coalg (AnnF (f, a)) = fromMaybe (alg f) (ann a)
-
--- para :: (Base t (t, a)  -> a) -> t -> a
-paraopto :: Functor f => (a -> Maybe b) -> (Fix f -> b) -> Ann f a -> b
-paraopto ann alg = para palg where
-  -- f :: ExprF (Ann ExprF a, b)
-  palg (AnnF (f,a)) = fromMaybe ( (alg . Fix)  (stripAll . fst <$> f) ) (ann a)
-  
-instance Show NamedExpr where
-  show = paraopto id alg . getNamed where
-    alg = show . Expr
+-- named :: String -> NamedExpr -> NamedExpr
+-- named s (Named (Fix (AnnF (e, _)))) = (Named . Fix . AnnF) (e, Just s)
+--
+-- unnamed :: ExprF (Ann ExprF (Maybe String)) -> NamedExpr
+-- unnamed e = (Named . Fix . AnnF) (e, Nothing)
+--
+-- evalName :: NamedExpr -> Integer
+-- evalName = eval . Expr . stripAll . getNamed
+--
+-- instance Num NamedExpr where
+--   Named a + Named b = unnamed (SumF a b)
+--   Named a * Named b = unnamed (PrdF a b)
+--   fromInteger = unnamed . CstF
+--   abs e | evalName e < 0 = negate e
+--         | otherwise      = e
+--   signum = fromInteger . signum . evalName
+--   negate = unnamed . NegF . getNamed
+--
+-- opto :: Functor f => (a -> Maybe b) -> (f b -> b) -> Ann f a -> b
+-- opto ann alg = cata coalg where
+--   coalg (AnnF (f, a)) = fromMaybe (alg f) (ann a)
+--
+-- -- para :: (Base t (t, a)  -> a) -> t -> a
+-- paraopto :: Functor f => (a -> Maybe b) -> (Fix f -> b) -> Ann f a -> b
+-- paraopto ann alg = para palg where
+--   -- f :: ExprF (Ann ExprF a, b)
+--   palg (AnnF (f,a)) = fromMaybe ( (alg . Fix)  (stripAll . fst <$> f) ) (ann a)
+--
+-- instance Show NamedExpr where
+--   show = paraopto id alg . getNamed where
+--     alg = show . Expr
   -- show = show . zygo void alg . getNamed where
   --   alg (AnnF (_, Just s)) = text s
   --   alg e@(AnnF (CstF i  ,_)) = integer i
