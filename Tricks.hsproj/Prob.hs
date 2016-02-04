e699e6497f3a698a795b363c6befa28589dc9192
oisdk <oisin.kidney@gmail.com>
2016-02-04 14:58:01 +0000

    props figured out

--- Tricks.hsproj/Prob.hs
+++ Tricks.hsproj/Prob.hs
@@ -25,33 +25,31 @@ equalProbs xs = probLength (fromIntegral $ length xs - 1) xs where
   probLength 0 (y:_) = Certainly y
   probLength n (y:ys) = (1 % n, y) :/: (probLength (n-1) ys)
   
+
+instance Show a => Show (Prob a) where
+  show = ('[':) . show' where
+    show' (Certainly x) = "(_ % _, " ++ show x ++ ")]"
+    show' (x :/: xs) = show x ++ ", " ++ show' xs
+
+conc :: Rational -> Prob a -> Prob a -> Prob a
+conc p (Certainly x) ys = (p,x) :/: ys
+conc io ((ii,y) :/: ys) zs = (oi, y) :/: (conc (io / (ii+1)) ys zs) where 
+  oi = io * ii / (io + ii + 1)
+
+flatten :: Prob (Prob a) -> Prob a
+flatten (Certainly xs) = xs
+flatten ((p,x) :/: xs) = conc p x (flatten xs)
+    
 instance Foldable Prob where
   foldMap f (Certainly x) = f x
   foldMap f ((_,x) :/: xs) = f x <> foldMap f xs
- 
-instance Show a => Show (Prob a) where
-  show = ('[':) . show' where
-    show' (Certainly x) = show x ++ "]"
-    show' (x :/: xs) = show x ++ ", " ++ show' xs
  
 instance Applicative Prob where
   pure = Certainly
   Certainly f <*> xs = f <$> xs
   ((p,f) :/: fs) <*> xs = conc p (f <$> xs) (fs <*> xs)
 
-conc :: Rational -> Prob a -> Prob a -> Prob a
-conc p (Certainly x) ys = (1,x) :/: ys
-conc p ((py,y) :/: ys) zs = (nn, y) :/: (conc no ys zs) where 
-  nn = p * py / (p + py + 1)
-  nd = (p / (p + 1)) - (nn / (nn + 1))
-  no = nd / (1 - nd)
-  
 
-    
-flatten :: Prob (Prob a) -> Prob a
-flatten (Certainly xs) = xs
-flatten ((p,x) :/: xs) = conc p x (flatten xs)
-    
 instance Monad Prob where
   return = Certainly
   x >>= f = flatten (fmap f x)
